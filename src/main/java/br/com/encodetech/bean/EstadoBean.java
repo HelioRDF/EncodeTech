84acd2b5dd7dfe839e1d8c23da0bcf8983168eae
HelioRDF <jpfranca@bol.com.br>
2016-11-15 22:35:05 -0200

    Atualização

--- src/main/java/br/com/encodetech/bean/EstadoBean.java
+++ src/main/java/br/com/encodetech/bean/EstadoBean.java
@@ -1,7 +1,11 @@
 package br.com.encodetech.bean;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
 import org.omnifaces.util.Messages;
+
+import br.com.encodetech.domain.localizacao.Estado;
 
 
 /**
@@ -14,13 +18,41 @@ import org.omnifaces.util.Messages;
  */
 
 @ManagedBean
+@SessionScoped
+
 public class EstadoBean {
+	
+	private Estado estado;
 	
 	
 	public void salvar(){
 		
-Messages.addGlobalInfo("ddd");
+		
+		
+Messages.addGlobalInfo("Codigo: "+estado.getCodigo());
+Messages.addGlobalInfo("Estado: "+estado.getNome());
+Messages.addGlobalInfo("Sigla: "+estado.getSigla());
 		
 	}
+	
+	public void novo(){
+		estado= new Estado();
+		
+	}
+	
+	
+	
+	//-------------------------------------------------
+
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	
 
 }
